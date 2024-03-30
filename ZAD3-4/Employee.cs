a71984ccab3a1dd6ab42a1dd54c0cbf384c943a0
popivus <909slender909@gmail.com>
2024-03-30 18:36:24 +0300

    Add project files.

---
+++ ZAD3-4/Employee.cs
@@ -0,0 +1,109 @@
+﻿namespace ZAD3_4
+{
+    public class Employee
+    {
+        private static int _employeeCount;
+        public static int CurrentEmployeCount => _employeeCount;
+
+        public Employee(string name, char gender, int age, string position, string organization, double experience, double salary)
+        {
+            Name = name;
+            Gender = gender;
+            Age = age;
+            Position = position;
+            Organization = organization;
+            Experience = experience;
+            Salary = salary;
+
+            _employeeCount++;
+        }
+
+        ~Employee() => _employeeCount--;
+
+        private int _age;
+        private double _experience;
+        private double _salary;
+
+        public string Name { get; private set; }
+
+        public char Gender { get; private set; }
+
+        public int Age 
+        { 
+            get => _age;
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Возраст не может быть меньше или равен 0");
+
+                _age = value;
+            }
+        }
+
+        public string Position { get; private set; }
+
+        public string Organization { get; private set; }
+
+        public double Experience 
+        { 
+            get => _experience;
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Стаж не может быть меньше 0");
+
+                _experience = value;
+            }
+        }
+
+        public double Salary 
+        { 
+            get => _salary;
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Зарплата не может быть меньше или равна 0");
+
+                _salary = value;
+            }
+        }
+
+        public string EmployeeInfo => $"Работает в {Organization}\n" +
+            $"На должности {Position}\n" +
+            $"Стаж: {Experience}\n" +
+            $"Зарплата: {Salary} руб.\n\n";
+
+        public void EditPosition(string newPosition) => Position = newPosition;
+
+        public void PaySalary(double money) => Salary += money;
+
+        public string PersonalData => $"Имя работника {Name}\n" +
+            $"Возраст {Age}\n" +
+            $"Пол: {Gender}";
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Employee emp)
+            {
+                if (Name == emp.Name &&
+                    Gender == emp.Gender &&
+                    Age == emp.Age &&
+                    Position == emp.Position &&
+                    Organization == emp.Organization &&
+                    Experience == emp.Experience &&
+                    Salary == emp.Salary)
+                    return true;
+
+                return false;
+            }
+
+            return false;
+        }
+
+        public static bool operator ==(Employee employee1, Employee employee2) => employee1.Equals(employee2);
+
+        public static bool operator !=(Employee employee1, Employee employee2) => !employee1.Equals(employee2);
+
+        public Employee Copy() => new Employee(Name, Gender, Age, Position, Organization, Experience, Salary);
+    }
+}
---
+++ ZAD3-4/Program.cs
@@ -0,0 +1,121 @@
+﻿using ZAD3_4;
+
+List<Employee> employees = InitEmployees().ToList();
+
+Console.Clear();
+while (true)
+{
+    Console.WriteLine($"Общее количество сотрудников: {Employee.CurrentEmployeCount}");
+
+    Employee currentEmployee = PickEmployee(employees, "Введите номер сотрудника, над которым необходимо произвести операцию: ");
+
+    Console.Clear();
+    Console.WriteLine("1 - Получить рабочую информацию");
+    Console.WriteLine("2 - Поменять должность");
+    Console.WriteLine("3 - Начислить з/п");
+    Console.WriteLine("4 - Получить личные данные");
+    Console.WriteLine("5 - Сравнить с другим сотрудников");
+    int operNumb = GetIntFromKeyboard("Введите номер операции: ", 1, 5);
+
+    Console.Clear();
+    switch(operNumb)
+    {
+        case 1:
+            Console.WriteLine(currentEmployee.EmployeeInfo);
+            Console.ReadKey();
+            break;
+        case 2:
+            string newPosition = GetStringFromKeyboard("Введите новую должность: ");
+            currentEmployee.EditPosition(newPosition);
+            break;
+        case 3:
+            double salary = GetDoubleFromKeyboard("Введите деньги: ");
+            currentEmployee.PaySalary(salary);
+            break;
+        case 4:
+            Console.WriteLine(currentEmployee.PersonalData);
+            Console.ReadKey();
+            break;
+        case 5:
+            Employee employeeToCompare = PickEmployee(employees, "Введите номер сотрудника, с которым необходимо сравнить: ");
+            Console.WriteLine();
+
+            if (currentEmployee == employeeToCompare)
+                Console.WriteLine("Это одинаковые сотрудники!");
+            else
+                Console.WriteLine("Это разные сотрудники!");
+            Console.ReadKey();
+
+            break;
+    }
+    Console.Clear();
+}
+
+
+Employee PickEmployee(List<Employee> employees, string message)
+{
+    for (int i = 0; i < employees.Count; i++)
+        Console.WriteLine($"{i + 1} - {employees[i].Name}");
+
+    int employeeNumber = GetIntFromKeyboard(message, 1, Employee.CurrentEmployeCount) - 1;
+
+    return employees[employeeNumber];
+}
+
+IEnumerable<Employee> InitEmployees()
+{
+    int employeesCount = GetIntFromKeyboard("Введите корректное количество работников: ", 1, int.MaxValue);
+
+    List<Employee> employees = new();
+    for (int i = 0; i < employeesCount; i++)
+    {
+        string name = GetStringFromKeyboard("Введите имя нового сотрудника: ");
+        char gender = GetIntFromKeyboard("Введите порядковый номер пола сотрудника (1 - М, 2 - Ж): ", 1, 2) == 1 ? 'М' : 'Ж';
+        int age = GetIntFromKeyboard("Введите возраст: ", 1, 150);
+        string position = GetStringFromKeyboard("Введите должность сотрудника: ");
+        string organization = GetStringFromKeyboard("Введите организацию сотрудника: ");
+        double experience = GetDoubleFromKeyboard("Введите стаж сотрудника: ");
+        double salary = GetDoubleFromKeyboard("Введите накопления сотрудника: ");
+        Console.WriteLine();
+
+        Employee employee = new Employee(name, gender, age, position, organization, experience, salary);
+        employees.Add(employee);
+    }
+
+    return employees;
+}
+
+
+string GetStringFromKeyboard(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
+
+double GetDoubleFromKeyboard(string message)
+{
+    string? str;
+    double number;
+    do
+    {
+        Console.Write(message);
+        str = Console.ReadLine();
+    }
+    while (!double.TryParse(str, out number) || number <= 0);
+
+    return number;
+}
+
+int GetIntFromKeyboard(string message, int min, int max)
+{
+    string? str;
+    int number;
+    do
+    {
+        Console.Write(message);
+        str = Console.ReadLine();
+    }
+    while (!int.TryParse(str, out number) || number < min || number > max);
+
+    return number;
+}
